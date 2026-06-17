39f7ba535193b4da51626d54b000458c2c1b02f9
Алейников Кирилл Андеевич <k_alejnikov@ormz.dc>
2026-06-17 12:47:42 +0700

    + CunnY func

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -242,13 +242,116 @@ namespace OCR_test
         // Там, где был текст (allTextMask), красим в черный цвет (0)
         result.SetTo(new Scalar(0), allTextMask);
 
+            CannY(result);
+
+
         var bytes3 = result.ToBytes(".jpg");
 
         return (bytes1, bytes2, bytes3);
 
     }
-    
 
+        static float Distance(Point2f p1, Point2f p2)
+        {
+            float dx = p2.X - p1.X;
+            float dy = p2.Y - p1.Y;
+
+            return MathF.Sqrt(dx * dx + dy * dy);
+        }
+        static Point2f[] OrderCorners(OpenCvSharp.Point[] pts)
+        {
+            var p = pts.Select(x => new Point2f(x.X, x.Y)).ToArray();
+
+            Point2f tl = p.OrderBy(v => v.X + v.Y).First();
+            Point2f br = p.OrderByDescending(v => v.X + v.Y).First();
+
+            Point2f tr = p.OrderBy(v => v.Y - v.X).First();
+            Point2f bl = p.OrderByDescending(v => v.Y - v.X).First();
+
+            return new[] { tl, tr, br, bl };
+        }
+
+        static void CannY(Mat src)
+        {
+            //var gray2 = new Mat();
+            //Cv2.CvtColor(src, gray2, ColorConversionCodes.BGR2GRAY);
+
+            //var blurred2 = new Mat();
+            //Cv2.GaussianBlur(src, blurred2, new OpenCvSharp.Size(5, 5), 0);
+
+            var edges2 = new Mat();
+            Cv2.Canny(src, edges2, 0, 0);
+            File.WriteAllBytes("EDGES.jpg", edges2.ToBytes(".jpg", new ImageEncodingParam(ImwriteFlags.JpegQuality, 100)));
+
+            Cv2.FindContours(
+                edges2,
+                out OpenCvSharp.Point[][] contours,
+                out HierarchyIndex[] hierarchy,
+                RetrievalModes.List,
+                ContourApproximationModes.ApproxSimple);
+
+
+            OpenCvSharp.Point[] screenContour = null;
+            double maxArea = 0;
+
+            foreach (var contour in contours)
+            {
+                double peri = Cv2.ArcLength(contour, true);
+
+                var approx = Cv2.ApproxPolyDP(
+                    contour,
+                    0.02 * peri,
+                    true);
+
+                if (approx.Length == 4)
+                {
+                    double area = Cv2.ContourArea(approx);
+
+                    if (area > maxArea)
+                    {
+                        maxArea = area;
+                        screenContour = approx;
+                    }
+                }
+            }
+
+
+            var corners = OrderCorners(screenContour);
+
+            float width =
+                Math.Max(
+                    Distance(corners[0], corners[1]),
+                    Distance(corners[2], corners[3]));
+
+            float height =
+                Math.Max(
+                    Distance(corners[0], corners[3]),
+                    Distance(corners[1], corners[2]));
+
+
+            var dstCorners = new[]
+                {
+                    new Point2f(0, 0),
+                    new Point2f(width - 1, 0),
+                    new Point2f(width - 1, height - 1),
+                    new Point2f(0, height - 1)
+                };
+
+            var matrix =
+                Cv2.GetPerspectiveTransform(
+                    corners,
+                    dstCorners);
+
+            var warped = new Mat();
+
+            Cv2.WarpPerspective(
+                src,
+                warped,
+                matrix,
+                new OpenCvSharp.Size((int)width, (int)height));
+
+            File.WriteAllBytes("WARPED.jpg", warped.ToBytes(".jpg"));
+        }
 
         public static List<(Tesseract.Rect, string)> Recognition(string TessDataPath, byte[] imgData)
         {
@@ -256,9 +359,9 @@ namespace OCR_test
             //string tessData = @"C:\tessdata\"; // Путь к данным Tesseract (скачать tessdata)
 
             var corrImgData = ProcessImage(imgData);
-            File.WriteAllBytes("postcorrection.png", corrImgData.Item1);
-            File.WriteAllBytes("postcorrectionN.png", corrImgData.Item2);
-            File.WriteAllBytes("postcorrectionSum.png", corrImgData.Item3);
+            //File.WriteAllBytes("postcorrection.png", corrImgData.Item1);
+            //File.WriteAllBytes("postcorrectionN.png", corrImgData.Item2);
+            //File.WriteAllBytes("postcorrectionSum.png", corrImgData.Item3);
 
             //var corrImgDataN = ProcessImageNeg(imgData);
             //File.WriteAllBytes("postcorrectionN.jpg", corrImgDataN);
@@ -268,6 +371,7 @@ namespace OCR_test
             using (var img = Pix.LoadFromMemory(corrImgData.Item3))
             using (var page = engine.Process(img))
             {
+                engine.SetVariable("tessedit_char_whitelist", "0123456789.,=:/-_ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZабвгдежзийклмнопрстуфхцчшщъыьэюяАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ");
                 using (var iter = page.GetIterator())
                 {
                     iter.Begin();
